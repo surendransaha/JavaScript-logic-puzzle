cd3a5214cc42be9ac2d43c2605911bb4a7fa1703
Suren <surendransaha@gmail.com>
2024-12-11 18:15:17 +0530

    Added while loop

---
+++ whileLoop.js
@@ -0,0 +1,11 @@
+
+// While Loop
+
+let arrayVal = [1,2,3,4,5,6,7,8];
+let index = 0;
+
+while(index < arrayVal.length){
+    console.log(arrayVal[index])
+    if(index == 3){ return false}
+    index++
+}
