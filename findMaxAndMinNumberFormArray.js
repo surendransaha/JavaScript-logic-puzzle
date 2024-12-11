340b4f74db3d92c34f035041e503b819822e0e5d
Suren <surendransaha@gmail.com>
2024-12-11 17:27:32 +0530

    max and min number find in JS array

---
+++ findMaxAndMinNumberFormArray.js
@@ -0,0 +1,7 @@
+let arrayNumber = [56, 78,1551, 2, 123, 89];
+
+let maxValue = Math.max(...arrayNumber);
+let minValue = Math.min(...arrayNumber);
+
+console.log("maxValue", maxValue);
+console.log("minValue", minValue);
