79eae62e3571bf931ce5c9199dccc7a505e1b55c
Suren <surendransaha@gmail.com>
2024-12-17 18:41:30 +0530

    for loop concept

---
+++ forLoop.js
@@ -0,0 +1,26 @@
+
+let arrayVal = ["alex", "mahi", "manu", "shree", "amir"];
+
+for(let i=0; i< arrayVal.length; i++){ // For Loop
+
+    console.log(arrayVal[i])
+
+}
+
+
+// For Of
+
+for(let data of arrayVal){
+    console.log(data)
+}
+
+
+
+
+//For In
+
+for(let data in arrayVal){
+
+    console.log(arrayVal[data])
+
+}
